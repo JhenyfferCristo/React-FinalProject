d2c5813993c5c7ccea5724abb08857da0f9af9ce
Jhenyffer Cristo <jhenyffercristo@gmail.com>
2023-10-25 14:11:41 -0600

    Routes implemented

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,22 +1,17 @@
+import React from 'react'
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Login } from './pages/login';
+import { Signup } from './pages/singup';
+import { Home } from './pages/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Routes>
+    <Route index element={<Home/>}/>
+   <Route path="login" element={<Login />}/>
+   <Route path="signup" element={<Signup/>}/>
+   </Routes>
   );
 }
 
---
+++ src/Components/pages/login.js
@@ -0,0 +1,9 @@
+export const Login=()=>{
+    return(
+       <>
+       
+        <h1>This is login page</h1>
+        </> 
+    )
+    
+}
