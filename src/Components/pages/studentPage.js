9452ec20a952cc8b53ff7cd748802281baa8b082
Bishal <124103977+Beesal21@users.noreply.github.com>
2023-10-30 03:36:23 -0600

    Update studentPage.js

--- src/Components/pages/studentPage.js
+++ src/Components/pages/studentPage.js
@@ -9,71 +9,40 @@ export const Student= () => {
         <h1>Student Support Services</h1> 
       </Row>
       
-      <Row>
-        <Col>
-          <h2>Academic Support</h2>
-          <p>
-            Get help from tutors, attend study skills workshops, meet with your program advisor - we offer a range of services to help you succeed academically.
-          </p>
-        </Col>
-        
-        <Col>
-          <h2>Career & Employment Support</h2>
-          <p>
-            From resume reviews to interview practice to job search strategies - our career center provides the tools and advising to help launch your career.
-          </p>
-        </Col>
-      </Row>
+    
+        // Create a function to generate content for each service
+        function createServiceElement(title, description) {
+            const serviceDiv = document.createElement('div');
+            const serviceTitle = document.createElement('h2');
+            const serviceDescription = document.createElement('p');
 
-      <Row>
-        <Col>
-          <h2>Early Support</h2>
-          <p>
-            Reach out for help early to get connected with resources and supports to get you back on track academically.  
-          </p>
-        </Col>
-        
-        <Col>
-          <h2>Financial Support</h2>
-          <p>
-            Speak to a financial aid advisor, apply for bursaries and grants, get help planning and budgeting - we're here to support your financial success.
-          </p>
-        </Col>
-      </Row>
-      
-      <Row>
-        <Col>
-          <h2>International Student Support</h2>
-          <p>
-            Find help transitioning to school in a new country. Connect with advisors, join the international student club and access resources to help you adjust.
-          </p>
-        </Col>
-        
-        <Col>
-          <h2>Learner Success Services</h2>
-          <p>
-            Workshops, advising and resources to help you develop strategies for time management, learning skills, test anxiety, organization and more.  
-          </p>
-        </Col>
-      </Row>
-      
-      <Row>
-        <Col>
-          <h2>Library</h2>
-          <p>
-            Access online resources, get research help, borrow books and more through the library to support your learning.
-          </p>
-        </Col>
-        
-        <Col>
-          <h2>Mental Health & Wellness</h2>
-          <p>
-            Counselling, wellness resources, peer support groups and health services - we're here to support your overall wellbeing.  
-          </p>
-        </Col>
-      </Row>
-      
-    </Container>
-  )
+            serviceTitle.textContent = title;
+            serviceDescription.textContent = description;
 
+            serviceDiv.appendChild(serviceTitle);
+            serviceDiv.appendChild(serviceDescription);
+
+            return serviceDiv;
+        }
+
+        // Define the services and their descriptions
+        const services = [
+            { title: 'Academic Support', description: 'Academic support services are available to help students succeed in their studies. These services may include tutoring, study groups, and academic advising.' },
+            { title: 'Career & Employment Support', description: 'Career and employment support services assist students in finding internships, job opportunities, and preparing for their careers.' },
+            { title: 'Early Support', description: 'Early support services are designed to help students who may be struggling academically or facing other challenges. They provide assistance and guidance to ensure student success.' },
+            { title: 'Financial Support', description: 'Financial support services offer information and assistance regarding scholarships, grants, loans, and financial aid to help students manage their finances while in school.' },
+            { title: 'International Student Support', description: 'International student support services cater to the unique needs of students from other countries, including visa support, cultural adjustment, and academic guidance.' },
+            { title: 'Learner Success Services', description: 'Learner success services provide resources and strategies to enhance the learning experience, such as time management and study skills.' },
+            { title: 'Library', description: 'The library offers access to a wide range of educational resources, including books, research materials, and study spaces for students to support their academic endeavors.' },
+            { title: 'Mental Health and Wellness', description: 'Mental health and wellness services focus on the emotional well-being of students, providing counseling and support for mental health issues.' },
+        ];
+
+        // Create a container to hold all the service elements
+        const container = document.getElementById('servicesContainer');
+
+        // Populate the container with service elements
+        services.forEach((service) => {
+            const serviceElement = createServiceElement(service.title, service.description);
+            container.appendChild(serviceElement);
+        });
 }
