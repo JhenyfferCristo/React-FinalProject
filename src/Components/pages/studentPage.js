ea721c29b121a70423fb3fcefc5e4325e957fec1
Bishal <124103977+Beesal21@users.noreply.github.com>
2023-10-30 03:39:15 -0600

    Update studentPage.js

--- src/Components/pages/studentPage.js
+++ src/Components/pages/studentPage.js
@@ -4,45 +4,58 @@ import { Container, Row, Col } from 'react-bootstrap';
 export const Student= () => {
 
   return (
+  
     <Container>
+      <h1>Student Services</h1>
       <Row>
-        <h1>Student Support Services</h1> 
+        <Col md={4}>
+          <h2>Academic Support</h2>
+          <p>Our academic support services are designed to help students succeed in their studies. We offer tutoring, study resources, and academic advising to assist you in your academic journey.</p>
+        </Col>
+        <Col md={4}>
+          <h2>Career & Employment Support</h2>
+          <p>We provide career guidance and support in finding employment opportunities. Our services include resume building, interview preparation, and job search assistance.</p>
+        </Col>
+        <Col md={4}>
+          <h2>Early Support</h2>
+          <p>Early support services are here to help students overcome challenges and ensure a successful start to their academic journey. Whether it's personal or academic support, we've got you covered.</p>
+        </Col>
       </Row>
-      
-    
-        // Create a function to generate content for each service
-        function createServiceElement(title, description) {
-            const serviceDiv = document.createElement('div');
-            const serviceTitle = document.createElement('h2');
-            const serviceDescription = document.createElement('p');
-
-            serviceTitle.textContent = title;
-            serviceDescription.textContent = description;
-
-            serviceDiv.appendChild(serviceTitle);
-            serviceDiv.appendChild(serviceDescription);
-
-            return serviceDiv;
-        }
-
-        // Define the services and their descriptions
-        const services = [
-            { title: 'Academic Support', description: 'Academic support services are available to help students succeed in their studies. These services may include tutoring, study groups, and academic advising.' },
-            { title: 'Career & Employment Support', description: 'Career and employment support services assist students in finding internships, job opportunities, and preparing for their careers.' },
-            { title: 'Early Support', description: 'Early support services are designed to help students who may be struggling academically or facing other challenges. They provide assistance and guidance to ensure student success.' },
-            { title: 'Financial Support', description: 'Financial support services offer information and assistance regarding scholarships, grants, loans, and financial aid to help students manage their finances while in school.' },
-            { title: 'International Student Support', description: 'International student support services cater to the unique needs of students from other countries, including visa support, cultural adjustment, and academic guidance.' },
-            { title: 'Learner Success Services', description: 'Learner success services provide resources and strategies to enhance the learning experience, such as time management and study skills.' },
-            { title: 'Library', description: 'The library offers access to a wide range of educational resources, including books, research materials, and study spaces for students to support their academic endeavors.' },
-            { title: 'Mental Health and Wellness', description: 'Mental health and wellness services focus on the emotional well-being of students, providing counseling and support for mental health issues.' },
-        ];
-
-        // Create a container to hold all the service elements
-        const container = document.getElementById('servicesContainer');
-
-        // Populate the container with service elements
-        services.forEach((service) => {
-            const serviceElement = createServiceElement(service.title, service.description);
-            container.appendChild(serviceElement);
-        });
-}
+      <Row>
+        <Col md={4}>
+          <h2>Financial Support</h2>
+          <p>We understand the financial challenges students face. Our financial support services offer guidance on scholarships, grants, and financial aid to make your education more affordable.</p>
+        </Col>
+        <Col md={4}>
+          <h2>Indigenous Student Services</h2>
+          <p>We provide specialized services for Indigenous students, including cultural support, academic guidance, and resources to help you thrive in your educational journey.</p>
+        </Col>
+        <Col md={4}>
+          <h2>Intercultural Centre</h2>
+          <p>The intercultural center promotes diversity and cultural exchange. It offers programs, events, and resources to foster understanding and connections among students from different backgrounds.</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={4}>
+          <h2>International Student Support</h2>
+          <p>For international students, we offer support services tailored to your unique needs, including visa assistance, cultural adjustment resources, and academic guidance.</p>
+        </Col>
+        <Col md={4}>
+          <h2>Learner Success Services</h2>
+          <p>Learner success services focus on enhancing your overall success as a student. We offer workshops, resources, and guidance to improve your learning experience.</p>
+        </Col>
+        <Col md={4}>
+          <h2>Library</h2>
+          <p>Our library is a valuable resource for students. Access a vast collection of books, research materials, and study spaces to support your academic endeavors.</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={4}>
+          <h2>Mental Health & Wellness</h2>
+          <p>We care about your mental health and well-being. Our services include counseling, mental health resources, and wellness programs to help you maintain a healthy life balance.</p>
+        </Col>
+        {/* Add more sections here as needed */}
+      </Row>
+    </Container>
+  );
+};
