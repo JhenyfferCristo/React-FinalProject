809a58c56d04e957ff2d07fc4faf2e8700283d7a
RafaScardini <rafaelscardini@gmail.com>
2023-11-09 15:25:03 -0700

    studentPage.js_Bishal

--- src/Components/pages/studentPage.js
+++ src/Components/pages/studentPage.js
@@ -1,61 +1,105 @@
 import React, { useState } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-
-export const Student= () => {
-
+import {
+  InputGroup,
+  Form,
+  Container,
+  Row,
+  Col,
+} from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import { CourseCard } from '../CourseCard';
+import { Link } from 'react-router-dom';
+export const Student = () => {
+  const courses = [
+    {
+      name: 'Project management 1',
+      code: 'Pr111',
+      department: 'Software Development',
+      program: 'Diploma',
+      startDate: '2024/01/01',
+      endDate: '2024/04/06',
+      term: '1',
+      fees: '3,000',
+      description:
+        'An introductory course designed to equip students with the fundamental principles and techniques of project management in the context of software development. This course provides a solid foundation for effectively planning, executing, and controlling software projects.',
+    },
+    {
+      name: 'Advanced Project management 1',
+      code: 'Pr333',
+      department: 'Software Development',
+      program: 'Diploma',
+      startDate: '2024/07/04',
+      endDate: '2024/12/23',
+      term: '3',
+      fees: '3,400',
+      description:
+        'An advanced-level course that builds upon the foundational principles of project management with a focus on complex projects in the field of software development. This course delves deeper into the intricacies of managing software projects, addressing advanced concepts and strategies.',
+    },
+    {
+      name: 'Networking',
+      code: 'Net222',
+      department: 'Software Development',
+      program: 'Diploma',
+      startDate: '2024/04/08',
+      endDate: '2024/08/18',
+      term: '2',
+      fees: '3,100',
+      description:
+        'An upper-level course designed to explore advanced project management principles and practices within the field of networking. This course is intended for students who have a solid foundation in project management and are looking to apply these skills to complex networking projects.',
+    },
+  ];
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const handleSearch = () => {
+    const filteredCourses = courses.filter((course) =>
+      course.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      course.code.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setSearchResults(filteredCourses);
+  };
   return (
-  
-    <Container>
-      <h1>Student Services</h1>
-      <Row>
-        <Col md={4}>
-          <h2>Academic Support</h2>
-          <p>Our academic support services are designed to help students succeed in their studies. We offer tutoring, study resources, and academic advising to assist you in your academic journey.</p>
-        </Col>
-        <Col md={4}>
-          <h2>Career & Employment Support</h2>
-          <p>We provide career guidance and support in finding employment opportunities. Our services include resume building, interview preparation, and job search assistance.</p>
-        </Col>
-        <Col md={4}>
-          <h2>Early Support</h2>
-          <p>Early support services are here to help students overcome challenges and ensure a successful start to their academic journey. Whether it's personal or academic support, we've got you covered.</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={4}>
-          <h2>Financial Support</h2>
-          <p>We understand the financial challenges students face. Our financial support services offer guidance on scholarships, grants, and financial aid to make your education more affordable.</p>
-        </Col>
-        <Col md={4}>
-          <h2>Indigenous Student Services</h2>
-          <p>We provide specialized services for Indigenous students, including cultural support, academic guidance, and resources to help you thrive in your educational journey.</p>
-        </Col>
-        <Col md={4}>
-          <h2>Intercultural Centre</h2>
-          <p>The intercultural center promotes diversity and cultural exchange. It offers programs, events, and resources to foster understanding and connections among students from different backgrounds.</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={4}>
-          <h2>International Student Support</h2>
-          <p>For international students, we offer support services tailored to your unique needs, including visa assistance, cultural adjustment resources, and academic guidance.</p>
-        </Col>
-        <Col md={4}>
-          <h2>Learner Success Services</h2>
-          <p>Learner success services focus on enhancing your overall success as a student. We offer workshops, resources, and guidance to improve your learning experience.</p>
-        </Col>
-        <Col md={4}>
-          <h2>Library</h2>
-          <p>Our library is a valuable resource for students. Access a vast collection of books, research materials, and study spaces to support your academic endeavors.</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={4}>
-          <h2>Mental Health & Wellness</h2>
-          <p>We care about your mental health and well-being. Our services include counseling, mental health resources, and wellness programs to help you maintain a healthy life balance.</p>
-        </Col>
-        {/* Add more sections here as needed */}
-      </Row>
-    </Container>
+<Container>
+<Row>
+<Col>
+<InputGroup size="md" className="mb-3">
+<Form.Control
+              placeholder="Search"
+              style={{ tabSize: '100px' }}
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+<InputGroup.Text className="bi bi-search" onClick={handleSearch}></InputGroup.Text>
+</InputGroup>
+</Col>
+</Row>
+<Row>
+<Col>
+<Link to="/studentPage/registerCourse">
+<Button variant="primary" type="submit">
+              Register for Course
+</Button>
+</Link>
+</Col>
+<Col>
+<Button variant="primary" type="submit">
+            Contact Form
+</Button>
+</Col>
+</Row>
+<Row>
+<h1>Available Courses</h1>
+</Row>
+<Row xs={1} md={2} lg={3}>
+        {searchResults.length > 0 ? (
+          searchResults.map((course, index) => (
+<CourseCard key={index} course={course} isAdminPage={false} />
+          ))
+        ) : (
+          courses.map((course, index) => (
+<CourseCard key={index} course={course} isAdminPage={false} />
+          ))
+        )}
+</Row>
+</Container>
   );
 };
