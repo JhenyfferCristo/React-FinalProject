8494254e64ff50eaa30f2ec874f689852df8d5e7
RafaScardini <rafaelscardini@gmail.com>
2023-11-09 17:49:41 -0700

    addCourse page improvements

--- src/Components/pages/addCourse.js
+++ src/Components/pages/addCourse.js
@@ -1,10 +1,11 @@
 import React, {useState} from 'react';
 import { Row } from 'react-bootstrap';
 import { Container, Form, Button, Col } from 'react-bootstrap';
+import { Formik } from 'formik';
+import * as yup from 'yup';
 
 
-const AddCourse = () => {
-    const [courseData, setCourseData] = useState({
+const INITIAL_VALUES = {
         courseName: '',
         startDate: '',
         endDate: '',
@@ -14,8 +15,24 @@ const AddCourse = () => {
         term:'',
         program:'',
         fees:'',
-      });
+      };
+
+      export const AddCourse = () => {
+        const [users, setUsers] = useState([]);
       
+        const signUpSchema = yup.object().shape({
+          courseName: yup.string().required(),
+          startDate: yup.date().required(),
+          endDate: yup.date().required(),
+          courseCode: yup.string().required(),
+          department: yup.string().required(),
+          description: yup.string().required(),
+          term: yup.string().required(),
+          program: yup.string().required(),
+          fees: yup.string().required(),
+        });
+      
+        /*
       const handleChange = (e) => {
         const { name, value } = e.target;
         setCourseData({ ...courseData, [name]: value });
@@ -25,81 +42,109 @@ const AddCourse = () => {
         e.preventDefault();
         // Handling the form submission
       };
+      */
 
 
+      console.log(users);
   return (
     <Container className="pt-5">
-        <Row>
+      <Row>
         <h3 className="text-center mb-5">Adding a New Course Form</h3>
-        </Row>
-      
+      </Row>
+
     <Row className="justify-content-center">
-     
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3">
-          <Form.Label>Course Name</Form.Label>
-          <Form.Control
-            type="text"
-            name="courseName"
-            value={courseData.courseName}
-            onChange={handleChange}
-            required
-          />
-        </Form.Group>
-        <Row className="mb-3">
+        <Col xs="10" lg="6">
+          <Formik
+            validationSchema={signUpSchema}
+            initialValues={INITIAL_VALUES}
+            onSubmit={(values, { resetForm }) => {
+              setUsers((prevUsers) => [...prevUsers, values]);
+              resetForm();
+            }}>
+            {({ values, handleChange, errors, touched, handleSubmit }) => {
+              function isInvalid(field) {
+                return Boolean(errors[field]) && Boolean(touched[field]);
+              }
+
+     return(
+      <Form noValidate onSubmit={handleSubmit}>
+                  <Row className="mb-3">
+                    <Form.Group as={Col} md="6">
+                      <Form.Label>Course name</Form.Label>
+                      <Form.Control
+                        required
+                        type="text"
+                        placeholder="Course name"
+                        name="courseName"
+                        value={values.courseName}
+                        onChange={handleChange}
+                        isInvalid={isInvalid('courseName')}
+                      />
+                    </Form.Group>
+                    </Row>
+
+                  <Row className="mb-3">
                     <Form.Group as={Col} md="6">
                       <Form.Label>Start Date</Form.Label>
                       <Form.Control
+                        required
                         type="date"
                         name="startDate"
-                        value={courseData.startDate}
+                        value={values.dob}
                         onChange={handleChange}
-                        required
+                        isInvalid={isInvalid('startDate')}
                       />
                     </Form.Group>
                     <Form.Group as={Col} md="6">
-                      <Form.Label>End Date</Form.Label>
+                    <Form.Label>End Date</Form.Label>
                       <Form.Control
+                        required
                         type="date"
                         name="endDate"
-                        value={courseData.endDate}
+                        value={values.dob}
                         onChange={handleChange}
-                        required
+                        isInvalid={isInvalid('endDate')}
                       />
                     </Form.Group>
                   </Row>
     
-        <Row className="mb-3">
-         <Form.Group as={Col} md="6">
-          <Form.Label>Course Code</Form.Label>
-          <Form.Control
-            type="text"
-            name="courseCode"
-            value={courseData.courseCode}
-            onChange={handleChange}
-            required
-          />
-        </Form.Group>
-        <Form.Group as={Col} md="6">
-          <Form.Label>Department</Form.Label>
-          <Form.Control
-            type="text"
-            name="department"
-            value={courseData.department}
-            onChange={handleChange}
-            required
-          />
-        </Form.Group>
-        </Row>
+                  <Row className="mb-3">
+                    <Form.Group as={Col} md="6">
+                      <Form.Label>Course code</Form.Label>
+                      <Form.Control
+                        required
+                        type="text"
+                        placeholder="Course code"
+                        name="courseCode"
+                        value={values.courseCode}
+                        onChange={handleChange}
+                        isInvalid={isInvalid('courseCode')}
+                      />
+                    </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label>Description</Form.Label>
+                    <Form.Group as={Col} md="6">
+                      <Form.Label>Department</Form.Label>
+                      <Form.Control
+                        required
+                        type="text"
+                        placeholder="Department"
+                        name="department"
+                        value={values.department}
+                        onChange={handleChange}
+                        isInvalid={isInvalid('department')}
+                      />
+                    </Form.Group>
+                  </Row>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Description</Form.Label>
           <Form.Control
-            as="textarea"
-            name="description"
-            value={courseData.description}
-            onChange={handleChange}
             required
+            type="textarea"
+            name="description"
+            value={values.description}
+            onChange={handleChange}
+            isInvalid={isInvalid('description')}
           />
         </Form.Group>
 
@@ -107,21 +152,23 @@ const AddCourse = () => {
          <Form.Group as={Col} md="6">
           <Form.Label>Term</Form.Label>
           <Form.Control
-            as="textarea"
-            name="term"
-            value={courseData.term}
-            onChange={handleChange}
             required
+            type="textarea"
+            name="term"
+            value={values.term}
+            onChange={handleChange}
+            isInvalid={isInvalid('term')}
           />
         </Form.Group>
         <Form.Group as={Col} md="6">
           <Form.Label>Program</Form.Label>
           <Form.Control
+            required
             as="textarea"
             name="program"
-            value={courseData.program}
+            value={values.program}
             onChange={handleChange}
-            required
+            isInvalid={isInvalid('program')}
           />
         </Form.Group>
         </Row>
@@ -129,21 +176,27 @@ const AddCourse = () => {
         <Form.Group className="mb-3">
           <Form.Label>Fees ($CAD)</Form.Label>
           <Form.Control
+            required
             as="textarea"
             name="fees"
-            value={courseData.fees}
+            value={values.fees}
             onChange={handleChange}
-            required
+            isInvalid={isInvalid('fees')}
           />
         </Form.Group>
         
-
-        <Button variant="primary" type="submit">
-          Add Course
-        </Button>
-       </Form>
-    </Row>
-  </Container>
+        <Row className="mt-4">
+                    <Button variant="primary" type="submit">
+                      Add Course
+                    </Button>
+                  </Row>
+  </Form>
+        );
+      }}
+    </Formik>
+  </Col>
+ </Row>
+</Container>
     
   );
 };
