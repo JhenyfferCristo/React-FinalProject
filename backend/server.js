b79cc4131fe92ea676a4e94c6aea264e989f5b88
Bishal <kunwarbishal563@gmail.com>
2023-12-08 04:28:01 -0700

    student page

--- backend/server.js
+++ backend/server.js
@@ -99,5 +99,84 @@ app.post("/login", async (req, res) => {
 //Rafael admin page
 
 //Bishal student page
+import express from "express";
+import cors from "cors";
+import fs from "fs";
+import { v4 as uuidv4 } from "uuid";
+
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+app.use(cors());
+
+// Fetch courses from the JSON file
+const getCourses = () => {
+  const coursesData = fs.readFileSync("courses.json", "utf-8");
+  return JSON.parse(coursesData).courses;
+};
+
+// Save updated user data to the JSON file
+const saveUsers = (userData) => {
+  try {
+    fs.writeFileSync("users.json", JSON.stringify(userData, null, 2));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Endpoint to fetch available courses
+app.get("/api/courses", (req, res) => {
+  try {
+    const courses = getCourses();
+    res.json(courses);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+// Endpoint to register for a course
+app.post("/api/register-course", (req, res) => {
+  try {
+    const { courseId, userId } = req.body; // Assuming frontend sends courseId and userId for registration
+
+    // Load users data from a JSON file (or database)
+    const usersData = fs.existsSync("users.json")
+      ? JSON.parse(fs.readFileSync("users.json", "utf-8"))
+      : [];
+
+    // Find the user by userId
+    const user = usersData.find((user) => user.id === userId);
+
+    if (user) {
+      // Check if the course exists in the courses data
+      const courses = getCourses();
+      const courseExists = courses.find((course) => course.code === courseId);
+
+      if (courseExists) {
+        // Add the courseId to the user's enrolledCourses (assuming it's an array in user object)
+        if (!user.enrolledCourses) {
+          user.enrolledCourses = [];
+        }
+        user.enrolledCourses.push(courseId);
+
+        // Save the updated user data
+        saveUsers(usersData);
+
+        res.json({ message: "Course registered successfully" });
+      } else {
+        res.status(404).json({ message: "Course not found" });
+      }
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 //Gabriele admin form and student form
